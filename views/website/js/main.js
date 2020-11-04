85acba15a87072721972b6a9ad3b50273591b945
Louis GALLET <46707587+louisgallet95@users.noreply.github.com>
2020-11-04 11:22:49 +0100

    add jquery

--- views/website/js/main.js
+++ views/website/js/main.js
@@ -1,12 +1,9 @@
 var nbPizza = 0;
 var nbBurger = 0;
-var fs = require('fs'), 
-	filePath = './js/countPizza.txt'; 
-var file = fs.readFileSync(filePath, 'utf8'); 
-console.log(css); 
-
 const pizza = document.getElementById("countPizza");
 const burger = document.getElementById("countBurger");
+
+
 
 function countPizza() {
   nbPizza = nbPizza + 1;
